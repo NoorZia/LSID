41f172ee840060fe9a9fcd2d6bd492f0832d5ae3
johansunds <31589286+johansunds@users.noreply.github.com>
2020-07-01 16:26:31 +0200

    Update histogram_equalization_test.py

--- histogram_equalization_test.py
+++ histogram_equalization_test.py
@@ -5,22 +5,13 @@ import rawpy
 from skimage import color, exposure
 from matplotlib import pyplot as plt
 
-data_path = r'E:\LSID\dataset'
-file_path = "Sony_test_list_reduced.txt"
-files = open(join(data_path, file_path), 'r').readlines()
-for i, f in enumerate(files):
-    if i == 200:
-        file_path_long = f.split(' ')[1]
-        break
-
-file_path_long = join(data_path, file_path_long)
+file_path_long = r'E:\LSID\results\histogram_eval\00100_00_30s.ARW'
 
 gt_image = rawpy.imread(file_path_long)
 gt_image = gt_image.postprocess(use_camera_wb=True, half_size=False, no_auto_bright=True, output_bps=16)
 
 # divide by 65535 after converting a uint16 intensity image to double
 target = np.float32(gt_image / 65535.0)  # RGB image shape (2848, 4256, 3), channel order: RGB
-
 
 # Convert image to HSV color space
 img_hsv = color.rgb2hsv(target)
@@ -34,11 +25,26 @@ img_equalized = color.hsv2rgb(img_hsv.astype('float'))
 
 plt.figure()
 plt.subplot(2, 2, 1)
+plt.title('Unmodified')
 plt.imshow(target)
+plt.axis('off')
+
 plt.subplot(2, 2, 2)
+plt.title('Histogram equalised')
 plt.imshow(img_equalized)
+plt.axis('off')
+
 plt.subplot(2, 2, 3)
-plt.hist(img_v.ravel())
-plt.subplot(2, 2, 4)
-plt.hist(img_hsv[:,:,2].ravel())
+plt.hist(img_v.ravel(), bins=256)
+plt.xlabel('Pixel intensity in HSV value channel')
+plt.ylabel('Number of pixels')
+ax = plt.gca()
+plt.gca().set_yticklabels([])
+
+plt.subplot(2, 2, 4, sharey=ax)
+plt.hist(img_hsv[:, :, 2].ravel(), bins=256)
+plt.xlabel('Pixel intensity in HSV value channel')
+plt.ylabel('Number of pixels')
+plt.gca().set_yticklabels([])
+
 plt.show()
